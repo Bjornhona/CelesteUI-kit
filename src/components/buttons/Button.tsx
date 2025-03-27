52d45e2c101372b03f94f20b856110cfa720468e
Bjornhona <asaeri3@hotmail.com>
2025-03-27 16:41:52 +0100

    variant and size set

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -2,33 +2,39 @@ import styles from './button.module.css';
 import { Button as ShadcnButton } from '@/components/ui/button';
 
 interface ButtonProps {
-  children: React.ReactNode;
-  type?: 'primary' | 'secondary' | 'tertiary';
-  backgroundColor?: string;
+  children?: React.ReactNode;
+  variant?: 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost' | 'link';
   size?: 'small' | 'medium' | 'large';
-  label: string;
-  onClick?: () => void;
+  label?: string;
+  // onClick?: () => void;
 }
 
 export const Button = ({
-  children,
-  type = 'primary',
+  // children,
+  variant = 'default',
   size = 'medium',
-  backgroundColor,
-  label,
-  onClick,
-  ...props
+  label = 'Celeste UI Button',
+  // onClick,
+  // ...props
 }: ButtonProps) => {
-  const uiButtonClass = [styles.celesteUiButton + type && `--${type}`, styles.celesteUiButton + size && `--${size}`].join(' ');
-  
+  // const uiButtonClass = [styles.celesteUiButton + variant && `__${variant}`, styles.celesteUiButton + size && `__${size}`].join(' ');
+  // const uiButtonClass = `${styles.celesteUiButton}${variant && `__${variant}`}${size && `__${size}`}`;
+  const buttonVariant = `celesteUiButton${variant && '__' + variant}`;
+  const buttonSize = `celesteUiButton${size && '__' + size}`;
+  // const uiButtonClass = styles[buttonStyle];
+
   return (
-    <ShadcnButton className={uiButtonClass} onClick={onClick} {...props}>
-      <style jsx>{`
-        button {
-          background-color: ${backgroundColor};
-        }
-      `}</style>
-      {label}{children}
+    <ShadcnButton
+      // asChild
+      className={[styles[buttonVariant], styles[buttonSize]].join(' ')}
+      // className={styles.celesteUiButton__primary}
+      // variant={variant}
+      // size={'sm'}
+      // onClick={onClick}
+      // {...props}
+    >
+      {label}
+      {/* {children && children} */}
     </ShadcnButton>
   );
 };
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,11 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+// import { fn } from '@storybook/test';
 
 import { Button } from '../components/buttons/Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
+  title: 'Celeste UI Button',
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
@@ -15,10 +15,10 @@ const meta = {
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    // backgroundColor: { control: 'color' },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
+  // args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -27,27 +27,56 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'Button',
+    variant: 'default',
+    label: 'Celeste UI Button'
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Button',
+    variant: 'secondary',
+    label: 'Celeste UI Button',
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    variant: 'outline',
+    label: 'Celeste UI Button',
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    variant: 'destructive',
+    label: 'Celeste UI Button',
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    variant: 'ghost',
+    label: 'Celeste UI Button',
+  },
+};
+
+export const Link: Story = {
+  args: {
+    variant: 'link',
+    label: 'Celeste UI Button',
   },
 };
 
 export const Large: Story = {
   args: {
     size: 'large',
-    label: 'Button',
+    label: 'Celeste UI Button',
   },
 };
 
 export const Small: Story = {
   args: {
     size: 'small',
-    label: 'Button',
+    label: 'Celeste UI Button',
   },
 };
